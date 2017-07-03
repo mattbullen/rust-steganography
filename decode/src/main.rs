5fd412c2f570f2550e99a5709eb4c8d922583625
Matthew Bullen <burner0110@gmail.com>
2017-07-02 21:41:32 -0700

    Update

--- decode/src/main.rs
+++ decode/src/main.rs
@@ -18,13 +18,13 @@ fn main() {
     let parts: Vec<&str> = split.collect();
     let key = parts[0];
     println!("Password: {}", key);
-	let width = parts[1].to_string().parse::<u32>().unwrap();
-	let height = parts[2].to_string().parse::<u32>().unwrap();
+    let width = parts[1].to_string().parse::<u32>().unwrap();
+    let height = parts[2].to_string().parse::<u32>().unwrap();
     let msg_length = parts[5].to_string().parse::<u8>().unwrap();
     println!("Message length: {} characters", msg_length);
-	let start_offset = parts[6].to_string().parse::<u32>().unwrap();
+    let start_offset = parts[6].to_string().parse::<u32>().unwrap();
     println!("Sequence offset: {}", start_offset);
-	let offset_max = parts[7].to_string().parse::<u32>().unwrap();
+    let offset_max = parts[7].to_string().parse::<u32>().unwrap();
     println!("Sequence max: {}", offset_max);
     let start_x = parts[3].to_string().parse::<u32>().unwrap();
     println!("Start X coordinate: {}", start_x);
@@ -34,81 +34,81 @@ fn main() {
     println!("Coordinate offsets (u8): {:?}", offsets);
     let source_file_name = &args[2];
     let source_image = image::open(&Path::new(source_file_name)).unwrap();
-	// let target = &args[3];
+    // let target = &args[3];
     // println!("Decoding: {}", target);
-	
-	let mut bin_list = Vec::new();
-	let mut source_pixels_counter :u32 = 0;
-	let mut source_cycle :u8 = 0;
-	let mut c_val :i32 = 0;
-	for (x, y, pixel) in source_image.pixels() {
-		if source_pixels_counter >= start_offset && source_pixels_counter < offset_max {
-			// println!("{:?} {} {}", pixel, source_pixels_counter, source_pixels_counter - start_offset);
-			c_val = pixel[source_cycle as usize] as i32;
-			if c_val.is_even() {
-				bin_list.push("0");
-			} else {
-				bin_list.push("1");
-			}
-			source_cycle += 1;
-			if source_cycle > 3 {
-				source_cycle = 0;
-			}
-		}
-		source_pixels_counter += 1;
+    
+    let mut bin_list = Vec::new();
+    let mut source_pixels_counter :u32 = 0;
+    let mut source_cycle :u8 = 0;
+    let mut c_val :i32 = 0;
+    for (x, y, pixel) in source_image.pixels() {
+        if source_pixels_counter >= start_offset && source_pixels_counter < offset_max {
+            // println!("{:?} {} {}", pixel, source_pixels_counter, source_pixels_counter - start_offset);
+            c_val = pixel[source_cycle as usize] as i32;
+            if c_val.is_even() {
+                bin_list.push("0");
+            } else {
+                bin_list.push("1");
+            }
+            source_cycle += 1;
+            if source_cycle > 3 {
+                source_cycle = 0;
+            }
+        }
+        source_pixels_counter += 1;
     }
-	// println!("Retrieved binary digits: {:?}", bin_list);
+    // println!("Retrieved binary digits: {:?}", bin_list);
 
-	let mut bin_assembled = Vec::new();
-	let mut bin_counter :u32 = 0;
-	for i in 0..bin_list.len() {
-		if bin_counter == 0 {
-			let mut bin_temp = Vec::new();
-			bin_temp.push(bin_list[i]);
-			bin_temp.push(bin_list[i + 1]);
-			bin_temp.push(bin_list[i + 2]);
-			bin_temp.push(bin_list[i + 3]);
-			bin_temp.push(bin_list[i + 4]);
-			bin_temp.push(bin_list[i + 5]);
-			bin_temp.push(bin_list[i + 6]);
-			bin_temp.push(bin_list[i + 7]);
-			let bin_string = bin_temp.join("");
-			bin_assembled.push(bin_string);
-		}
-		bin_counter += 1;
-		if bin_counter > 7 {
-			bin_counter = 0;
-		}
-	}
-	// println!("Retrieved binary pixels: {:?}", bin_assembled);
-	
-	let pixel_vals: Vec<_> = bin_assembled.into_iter().map(|x| u32::from_str_radix(&x, 2).unwrap()).collect();
-	// println!("Pixel channel values: {:?}", pixel_vals);
-	println!("Number of pixel channels: {}", pixel_vals.len());
-	
-	let mut val_cycle :u8 = 0;
-	let mut final_pixels_list = Vec::new();
-	let mut new_pixel = image::Rgba([0u8, 0u8, 0u8, 0u8]);
-	for i in 0..pixel_vals.len() {
-		if val_cycle == 0 {
-			new_pixel = image::Rgba([pixel_vals[i] as u8, pixel_vals[i + 1] as u8, pixel_vals[i + 2] as u8, pixel_vals[i + 3] as u8]);
-			final_pixels_list.push(new_pixel);
-		}
-		val_cycle += 1;
-		if val_cycle > 3 {
+    let mut bin_assembled = Vec::new();
+    let mut bin_counter :u32 = 0;
+    for i in 0..bin_list.len() {
+        if bin_counter == 0 {
+            let mut bin_temp = Vec::new();
+            bin_temp.push(bin_list[i]);
+            bin_temp.push(bin_list[i + 1]);
+            bin_temp.push(bin_list[i + 2]);
+            bin_temp.push(bin_list[i + 3]);
+            bin_temp.push(bin_list[i + 4]);
+            bin_temp.push(bin_list[i + 5]);
+            bin_temp.push(bin_list[i + 6]);
+            bin_temp.push(bin_list[i + 7]);
+            let bin_string = bin_temp.join("");
+            bin_assembled.push(bin_string);
+        }
+        bin_counter += 1;
+        if bin_counter > 7 {
+            bin_counter = 0;
+        }
+    }
+    // println!("Retrieved binary pixels: {:?}", bin_assembled);
+    
+    let pixel_vals: Vec<_> = bin_assembled.into_iter().map(|x| u32::from_str_radix(&x, 2).unwrap()).collect();
+    // println!("Pixel channel values: {:?}", pixel_vals);
+    println!("Number of pixel channels: {}", pixel_vals.len());
+    
+    let mut val_cycle :u8 = 0;
+    let mut final_pixels_list = Vec::new();
+    let mut new_pixel = image::Rgba([0u8, 0u8, 0u8, 0u8]);
+    for i in 0..pixel_vals.len() {
+        if val_cycle == 0 {
+            new_pixel = image::Rgba([pixel_vals[i] as u8, pixel_vals[i + 1] as u8, pixel_vals[i + 2] as u8, pixel_vals[i + 3] as u8]);
+            final_pixels_list.push(new_pixel);
+        }
+        val_cycle += 1;
+        if val_cycle > 3 {
             val_cycle = 0;
         }
-	}
-	// println!("Reassembled pixels: {:?}", final_pixels_list);
-	println!("Number of reassembled pixels: {}", final_pixels_list.len());
-	
-	let mut decode_image = image::ImageBuffer::new(width, height);
-	let mut pixel_counter :u32 = 0;
-	for (x, y, pixel) in decode_image.enumerate_pixels_mut() {
-		*pixel = final_pixels_list[pixel_counter as usize];
-		pixel_counter += 1;
-	}
-	
+    }
+    // println!("Reassembled pixels: {:?}", final_pixels_list);
+    println!("Number of reassembled pixels: {}", final_pixels_list.len());
+    
+    let mut decode_image = image::ImageBuffer::new(width, height);
+    let mut pixel_counter :u32 = 0;
+    for (x, y, pixel) in decode_image.enumerate_pixels_mut() {
+        *pixel = final_pixels_list[pixel_counter as usize];
+        pixel_counter += 1;
+    }
+    
     // Decode the message from the image pixel RGBA channels.
     let mut cycle :u8 = 0;
     let mut x_location :u8 = 0;
@@ -134,24 +134,24 @@ fn main() {
             x_location = map_index;
         }
         x_location = offsets[x_location as u32 as usize];
-		
-		/*
-		if target == "raw" {
-			let pixel = raw_source_image.get_pixel(x_location as u32 + start_x, i as u32 + start_y);
-			item = pixel[cycle as usize] as u8;
-			retrieved_characters.push(item);
-		} else {
-			let pixel = decode_image.get_pixel(x_location as u32 + start_x, i as u32 + start_y);
-			item = pixel[cycle as usize] as u8;
-			retrieved_characters.push(item);
-		}
-		*/
-		
-		let pixel = decode_image.get_pixel(x_location as u32 + start_x, i as u32 + start_y);
-		item = pixel[cycle as usize] as u8;
-		retrieved_characters.push(item);
-		
-		cycle += 1;
+        
+        /*
+        if target == "raw" {
+            let pixel = raw_source_image.get_pixel(x_location as u32 + start_x, i as u32 + start_y);
+            item = pixel[cycle as usize] as u8;
+            retrieved_characters.push(item);
+        } else {
+            let pixel = decode_image.get_pixel(x_location as u32 + start_x, i as u32 + start_y);
+            item = pixel[cycle as usize] as u8;
+            retrieved_characters.push(item);
+        }
+        */
+        
+        let pixel = decode_image.get_pixel(x_location as u32 + start_x, i as u32 + start_y);
+        item = pixel[cycle as usize] as u8;
+        retrieved_characters.push(item);
+        
+        cycle += 1;
         if cycle > 3 {
             cycle = 0;
         }
--- encode/src/main.rs
+++ encode/src/main.rs
@@ -18,18 +18,18 @@ fn main() {
     // Capture the command line arguments.
     let args: Vec<String> = env::args().collect();
     // println!("Parameters supplied: {:?}", args);
-	let key = &args[3];
-	println!("Password: {}", key);
+    let key = &args[3];
+    println!("Password: {}", key);
     let width = args[1].parse::<u32>().unwrap();
-	println!("Encoding image width: {}px", width);
+    println!("Encoding image width: {}px", width);
     let height = args[2].parse::<u32>().unwrap();
     println!("Encoding image height: {}px", height);
     let text_file_name = &args[4];
-	println!("Message source: {}", text_file_name);
-	let mask_file = &args[5];
-	println!("Destination image: {}", mask_file);
+    println!("Message source: {}", text_file_name);
+    let mask_file = &args[5];
+    println!("Destination image: {}", mask_file);
     let toggle_save_raw = &args[6];
-	
+    
     // Generate an image filled with random RGBA pixel channel values.
     let mut generated_image = image::ImageBuffer::new(width, height);
     let mut r :u8 = 0;
@@ -71,7 +71,7 @@ fn main() {
     let mut reset :f32 = 0.0;
     let mut reset_u8 :u8 = 0;
     let mut reset_multiplier :u8 = 0;
-	let mut pixel;
+    let mut pixel;
     for (i, item) in msg.iter().enumerate() {
         
         let map_index = i as u8;
@@ -106,145 +106,145 @@ fn main() {
         if cycle == 2 {
             pixel = image::Rgba([r, g, *item as u8, a]);
         }
-		if cycle == 3 {
+        if cycle == 3 {
             pixel = image::Rgba([r, g, b, *item as u8]);
         }
         generated_image.put_pixel(x_location as u32 + raw_start_x, i as u32 + raw_start_y, pixel);
         cycle += 1;
     }
-	
-	// Masking println!("y: {}", y); 
-	
-	let mut channel_values_list = Vec::new();
-	for (x, y, pixel) in generated_image.enumerate_pixels_mut() {
-		channel_values_list.push(pixel[0]);
-		channel_values_list.push(pixel[1]);
-		channel_values_list.push(pixel[2]);
-		channel_values_list.push(pixel[3]);
+    
+    // Masking println!("y: {}", y); 
+    
+    let mut channel_values_list = Vec::new();
+    for (x, y, pixel) in generated_image.enumerate_pixels_mut() {
+        channel_values_list.push(pixel[0]);
+        channel_values_list.push(pixel[1]);
+        channel_values_list.push(pixel[2]);
+        channel_values_list.push(pixel[3]);
     }
-	// println!("Channel values list: {:?}", channel_values_list); 
-	
-	let bin_values: Vec<_> = channel_values_list.into_iter().map(|x| format!("{:b}", x)).map(|x| x.to_string()).collect();
-	// println!("Binary values list: {:?}", bin_values);
-	
-	let mut padded_bin = Vec::new();
-	for i in 0..bin_values.len() {
-		let ref bin = bin_values[i];
-		let padded = format!("00000000{}", bin);
-		let iter_bin: Vec<char> = padded.chars().rev().take(8).collect();
-		
-		//println!("{} {:?}", padded, iter_bin);
-		
-		padded_bin.push(iter_bin[7]);
-		padded_bin.push(iter_bin[6]);
-		padded_bin.push(iter_bin[5]);
-		padded_bin.push(iter_bin[4]);
-		padded_bin.push(iter_bin[3]);
-		padded_bin.push(iter_bin[2]);
-		padded_bin.push(iter_bin[1]);
-		padded_bin.push(iter_bin[0]);
-	}
-	// println!("{:?} {}", padded_bin, padded_bin.len());
-	
-	let mask_image = image::open(&Path::new(mask_file)).unwrap();
-	let channel_values_length = padded_bin.len();
-	let channel_values_length_u32 = channel_values_length as u32;
-	let (mask_width, mask_height) = mask_image.dimensions();
-	let sequence_max = (mask_height * mask_width) - channel_values_length_u32;
-	
-	let sequence_offset = rand::thread_rng().gen_range(0, sequence_max);
-	println!("Sequence offset: {}", sequence_offset);
-	let sequence_offset_max = sequence_offset + channel_values_length_u32;
-	println!("Sequence offset max: {}", sequence_offset_max);
-	
-	let mut final_values_list = Vec::new();
-	let mut mask_counter :u32 = 0;
-	let mut cycle :u8 = 0;
-	let mut bin_val :char = 0 as char;
-	let mut cycle_usize = cycle as usize;
-	let mut c_val_int :i32 = 0;
-	let mut c_val :u8 = 0;
-	let mut combined_pixel = image::Rgba([0u8, 0u8, 0u8, 0u8]);
-	for (x, y, pixel) in mask_image.pixels() {
-		
-		if mask_counter >= sequence_offset && mask_counter < sequence_offset_max {
-			
-			// println!("{} {} {}", mask_counter, mask_counter - sequence_offset, cycle);
-			// println!("{:?} {}", pixel, pixel[cycle as usize]);
-			
-			bin_val = padded_bin[(mask_counter - sequence_offset) as usize];
-			cycle_usize = cycle as usize;
-				
-			c_val_int = pixel[cycle_usize] as i32;
-		    c_val = pixel[cycle_usize] as u8;
-			if bin_val == 49 as char {
-				if c_val_int.is_even() {
-					c_val = (pixel[cycle_usize] as u8) + 1;
-				}
-			} else {
-				if c_val_int.is_odd() {
-					if c_val == 255 { 
-						c_val = 254 as u8; 
-					} else { 
-						c_val = (pixel[cycle_usize] as u8) + 1; 
-					}
-				}
-			}
-				
-			if cycle == 0 {
-				combined_pixel = image::Rgba([c_val, pixel[1], pixel[2], pixel[3]]);
-				final_values_list.push(combined_pixel);
-			}
-				
-			if cycle == 1 {
-				combined_pixel = image::Rgba([pixel[0], c_val, pixel[2], pixel[3]]);
-				final_values_list.push(combined_pixel);
-			}
-				
-			if cycle == 2 {
-				combined_pixel = image::Rgba([pixel[0], pixel[1], c_val, pixel[3]]);
-				final_values_list.push(combined_pixel);
-			}
-				
-			if cycle == 3 {
-				combined_pixel = image::Rgba([pixel[0], pixel[1], pixel[2], c_val]);
-				final_values_list.push(combined_pixel);
-			}
-				
-			cycle += 1;
-			if cycle > 3 {
-				cycle = 0;
-			}	
-		
-		} else {
-			combined_pixel = image::Rgba([pixel[0], pixel[1], pixel[2], pixel[3]]);
-			final_values_list.push(combined_pixel);
-		}
-		
-		mask_counter += 1;
-		
+    // println!("Channel values list: {:?}", channel_values_list); 
+    
+    let bin_values: Vec<_> = channel_values_list.into_iter().map(|x| format!("{:b}", x)).map(|x| x.to_string()).collect();
+    // println!("Binary values list: {:?}", bin_values);
+    
+    let mut padded_bin = Vec::new();
+    for i in 0..bin_values.len() {
+        let ref bin = bin_values[i];
+        let padded = format!("00000000{}", bin);
+        let iter_bin: Vec<char> = padded.chars().rev().take(8).collect();
+        
+        //println!("{} {:?}", padded, iter_bin);
+        
+        padded_bin.push(iter_bin[7]);
+        padded_bin.push(iter_bin[6]);
+        padded_bin.push(iter_bin[5]);
+        padded_bin.push(iter_bin[4]);
+        padded_bin.push(iter_bin[3]);
+        padded_bin.push(iter_bin[2]);
+        padded_bin.push(iter_bin[1]);
+        padded_bin.push(iter_bin[0]);
     }
-	// println!("Final pixels list: {:?}", final_values_list);
-	
-	let mut final_image = image::ImageBuffer::new(mask_width, mask_height);
-	let mut final_counter :u32 = 0;
-	for (x, y, pixel) in final_image.enumerate_pixels_mut() {
-		*pixel = final_values_list[final_counter as usize];
-		final_counter += 1;
-	}
+    // println!("{:?} {}", padded_bin, padded_bin.len());
+    
+    let mask_image = image::open(&Path::new(mask_file)).unwrap();
+    let channel_values_length = padded_bin.len();
+    let channel_values_length_u32 = channel_values_length as u32;
+    let (mask_width, mask_height) = mask_image.dimensions();
+    let sequence_max = (mask_height * mask_width) - channel_values_length_u32;
+    
+    let sequence_offset = rand::thread_rng().gen_range(0, sequence_max);
+    println!("Sequence offset: {}", sequence_offset);
+    let sequence_offset_max = sequence_offset + channel_values_length_u32;
+    println!("Sequence offset max: {}", sequence_offset_max);
+    
+    let mut final_values_list = Vec::new();
+    let mut mask_counter :u32 = 0;
+    let mut cycle :u8 = 0;
+    let mut bin_val :char = 0 as char;
+    let mut cycle_usize = cycle as usize;
+    let mut c_val_int :i32 = 0;
+    let mut c_val :u8 = 0;
+    let mut combined_pixel = image::Rgba([0u8, 0u8, 0u8, 0u8]);
+    for (x, y, pixel) in mask_image.pixels() {
+        
+        if mask_counter >= sequence_offset && mask_counter < sequence_offset_max {
+            
+            // println!("{} {} {}", mask_counter, mask_counter - sequence_offset, cycle);
+            // println!("{:?} {}", pixel, pixel[cycle as usize]);
+            
+            bin_val = padded_bin[(mask_counter - sequence_offset) as usize];
+            cycle_usize = cycle as usize;
+                
+            c_val_int = pixel[cycle_usize] as i32;
+            c_val = pixel[cycle_usize] as u8;
+            if bin_val == 49 as char {
+                if c_val_int.is_even() {
+                    c_val = (pixel[cycle_usize] as u8) + 1;
+                }
+            } else {
+                if c_val_int.is_odd() {
+                    if c_val == 255 { 
+                        c_val = 254 as u8; 
+                    } else { 
+                        c_val = (pixel[cycle_usize] as u8) + 1; 
+                    }
+                }
+            }
+                
+            if cycle == 0 {
+                combined_pixel = image::Rgba([c_val, pixel[1], pixel[2], pixel[3]]);
+                final_values_list.push(combined_pixel);
+            }
+                
+            if cycle == 1 {
+                combined_pixel = image::Rgba([pixel[0], c_val, pixel[2], pixel[3]]);
+                final_values_list.push(combined_pixel);
+            }
+                
+            if cycle == 2 {
+                combined_pixel = image::Rgba([pixel[0], pixel[1], c_val, pixel[3]]);
+                final_values_list.push(combined_pixel);
+            }
+                
+            if cycle == 3 {
+                combined_pixel = image::Rgba([pixel[0], pixel[1], pixel[2], c_val]);
+                final_values_list.push(combined_pixel);
+            }
+                
+            cycle += 1;
+            if cycle > 3 {
+                cycle = 0;
+            }   
+        
+        } else {
+            combined_pixel = image::Rgba([pixel[0], pixel[1], pixel[2], pixel[3]]);
+            final_values_list.push(combined_pixel);
+        }
+        
+        mask_counter += 1;
+        
+    }
+    // println!("Final pixels list: {:?}", final_values_list);
+    
+    let mut final_image = image::ImageBuffer::new(mask_width, mask_height);
+    let mut final_counter :u32 = 0;
+    for (x, y, pixel) in final_image.enumerate_pixels_mut() {
+        *pixel = final_values_list[final_counter as usize];
+        final_counter += 1;
+    }
 
-	// Output 
-	let masked_name = format!("encoded_{}", mask_file);
-		let ref mut mask_fout = File::create(&Path::new(&masked_name)).unwrap();
-		let _ = image::ImageRgba8(final_image).save(mask_fout, image::PNG);
-		println!("Message saved to: {}", &masked_name);
-	
-	if toggle_save_raw == "raw" {
-		let raw_name = format!("raw_encoded_{}", mask_file);
-		let ref mut raw_fout = File::create(&Path::new(&raw_name)).unwrap();
-		let _ = image::ImageRgba8(generated_image).save(raw_fout, image::PNG);
-		println!("Encoding image file: {}", &raw_name);
-	}
+    // Output 
+    let masked_name = format!("encoded_{}", mask_file);
+        let ref mut mask_fout = File::create(&Path::new(&masked_name)).unwrap();
+        let _ = image::ImageRgba8(final_image).save(mask_fout, image::PNG);
+        println!("Message saved to: {}", &masked_name);
+    
+    if toggle_save_raw == "raw" {
+        let raw_name = format!("raw_encoded_{}", mask_file);
+        let ref mut raw_fout = File::create(&Path::new(&raw_name)).unwrap();
+        let _ = image::ImageRgba8(generated_image).save(raw_fout, image::PNG);
+        println!("Encoding image file: {}", &raw_name);
+    }
 
     let decode_password = format!("{}*{}*{}*{}*{}*{}*{}*{}", key, width, height, raw_start_x, raw_start_y, msg_length, sequence_offset, sequence_offset_max);
     println!("To decode: {}", decode_password);
